958dcc1cf5724f11b3592be9a692337bc4fa07c4
koichi <k.hagio519@gmail.com>
2022-06-06 14:38:43 +0000

    6.2.2

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   
   get    '/login', to: 'sessions#new'
-  post   '/login', to: 'session#create'
-  delete '/logout', to: 'session#destroy'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
