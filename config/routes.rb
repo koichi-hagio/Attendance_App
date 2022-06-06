65202bcb62d70ee21b1b0829f8f7d27281b45a90
koichi <k.hagio519@gmail.com>
2022-06-06 14:15:52 +0000

    6.2.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+  
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+
   resources :users
 end
