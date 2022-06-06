56bf930b33c79b62e7e128e61249308ce657f10a
koichi <k.hagio519@gmail.com>
2022-06-06 22:55:50 +0000

    6.2.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ログイン後にユーザー情報ページにリダイレクトします。
     else 
+      flash.now[:danger] = "認証に失敗しました。"
     render :new
     end
   end
