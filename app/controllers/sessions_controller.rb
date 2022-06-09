373be1c7de38b79da57d0a1fa29244ce824a0ccd
koichi <k.hagio519@gmail.com>
2022-06-09 00:01:28 +0000

    7.1.3_3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       remember user
       redirect_to user  
     else 
-      flash.now[:danger] = "認証に失敗しました。"
+      flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ApplicationRecord
+  # 「remember_token」という仮想の属性を作成します。
+    attr_accessor :remember_token
     before_save { self.email = email.downcase }
     
-    validates :name, presence: true, length: {maximum: 50}
+    validates :name, presence: true, length: { maximum: 50 }
+    
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 100},
-                      format: {with: VALID_EMAIL_REGEX},
+                      format: { with: VALID_EMAIL_REGEX },
                       uniqueness: true
     has_secure_password
-    validates :password, presence: true, length: {minimum: 6}
+    validates :password, presence: true, length:{ minimum: 6 }
     
   # 渡された文字列のハッシュ値を返します。
   def User.digest(string)
